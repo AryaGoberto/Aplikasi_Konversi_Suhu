1c81fbfa4919482a8fd8f9f763b94266325e218a
AryaGoberto <arguna2019@gmail.com>
2025-05-13 23:21:47 +0800

    fix error

--- app/src/main/java/com/example/codeerrrr/MainActivity.kt
+++ app/src/main/java/com/example/codeerrrr/MainActivity.kt
@@ -2,21 +2,26 @@ package com.example.codeerrrr
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var spinnerFrom: Spinner
     private lateinit var spinnerTo: Spinner
+    private lateinit var resultText: TextView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // ⬅️ Panggil ini dulu!
+        setContentView(R.layout.activity_main)
 
         spinnerFrom = findViewById(R.id.spinnerFrom)
         spinnerTo = findViewById(R.id.spinnerTo)
+        resultText = findViewById(R.id.hasilText)
 
         val units = resources.getStringArray(R.array.suhu_array)
 
@@ -24,8 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         adapterUnit.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-        spinnerFrom.adapter = adapterUnit // ⬅️ Tampilkan isinya
+        spinnerFrom.adapter = adapterUnit
         spinnerTo.adapter = adapterUnit
-
     }
 }
